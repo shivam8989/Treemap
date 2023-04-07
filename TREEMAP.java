1f4adcfcc479ba648a9db97839a9e9d20a81a3c8
SHIVAM SINGH <91870115+shivam8989@users.noreply.github.com>
2023-04-07 09:59:31 +0530

    Add files via upload
    Implementation of treemap in java

---
+++ TREEMAP.java
@@ -0,0 +1,12 @@
+package HASHING;
+import java.util.*;
+public class TREEMAP {
+    public static void main(String args[]){
+        TreeMap<String , Integer> tm = new TreeMap<>();
+        tm.put("India",88);
+        tm.put("China",89);
+        tm.put("Usa",90);
+        System.out.println(tm);
+
+    }
+}
